6e3432c691bd11ce7346b811f2f6b7140f0c4c31
Adya Naufal Fikri <adyanaufalf@gmail.com>
2020-09-28 21:58:05 +0700

    fix import isUniqueSolutionTest

--- src/com/adyanf/ctci_solution/array/IsUniqueSolutionTest.kt
+++ src/com/adyanf/ctci_solution/array/IsUniqueSolutionTest.kt
@@ -1,6 +1,6 @@
 package com.adyanf.ctci_solution.array
 
-import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 internal class IsUniqueSolutionTest {
